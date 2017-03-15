06578229a8a822c0ca5d1d0cfabda1ae9ad8b56e
Michael Hammond <Michael Hammond>
2017-03-15 13:25:13 -0400

    The Project

---
+++ Level Revolution/AudioFormat.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Music here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import javax.sound.sampled.*;
+public class AudioFormat
+{
+    public static void main(String[] args)
+    {
+         AudioFormat music = new AudioFormat(Final Round Tournament.mp3);
+    }
+}
